872997e6559360bc67481027aba341541e2d17da
Matt <mattmartin14@gmail.com>
2023-07-20 13:56:11 -0400

    updated the go csv writer

--- go_code/dummy_data_writer/go_dummy_data_writer.go
+++ go_code/dummy_data_writer/go_dummy_data_writer.go
@@ -4,6 +4,13 @@ package main
 	Author: Matt Martin
 	Date: 2023-07-20
 	Desc: Writes a bunch of dummy data to a CSV File
+
+	TO DO:
+		1) Redirect the output to a non-git folder
+		2) create a random value generator as a column
+		3) have it pick from random date range
+		4) have it pick from random list of strings (from slice)
+
 */
 
 import (
@@ -30,12 +37,12 @@ func main() {
 	headers := []string{"dummy_header1", "dummy_header2", "dummy_header3"}
 	writer.Write(headers)
 
-	max_iterations := 1000
+	max_iterations := 100000000
 
 	for i := 1; i <= max_iterations; i++ {
 		rec := []string{"test", strconv.Itoa(i), "test2"}
 		writer.Write(rec)
-		fmt.Println(i)
+		//fmt.Println(i)
 	}
 
 }
