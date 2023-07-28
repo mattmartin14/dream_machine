44625635b9e89e7cc153436f355f7b17afe63d6f
Matt <mattmartin14@gmail.com>
2023-07-27 21:02:37 -0400

    updated sql script with params

--- duckdb/small_data_pipeline/csv_parquet_tsfm.sh
+++ duckdb/small_data_pipeline/csv_parquet_tsfm.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-sql1=$(cat "tsfm.sql")
-duckdb -c "$sql1"
+source_file="~/test_dummy_data/dummy_data2.csv"
+dest_file="~/test_dummy_data/people_agg_tsfm_v2.parquet"
+sql=$(cat "tsfm.sql")
+
+## update params
+sql=$(echo "$sql" | awk -v find="@source_file" -v replace="$source_file" '{ gsub(find, replace); print }')
+sql=$(echo "$sql" | awk -v find="@dest_file" -v replace="$dest_file" '{ gsub(find, replace); print }')
+#echo "$sql"
+duckdb -c "$sql"
--- duckdb/small_data_pipeline/tsfm.sql
+++ duckdb/small_data_pipeline/tsfm.sql
@@ -1,7 +1,7 @@
 
 -- load base data
 CREATE TABLE data_raw as
-SELECT * FROM read_csv_auto('~/test_dummy_data/dummy_data2.csv')
+SELECT * FROM read_csv_auto('@source_file')
 ;
 
 -- get total occurances of a full name and filter for top 10
@@ -21,10 +21,10 @@ QUALIFY ranked_cnt BETWEEN 1 AND 5
 ;
 
 -- write out to parquet file
-COPY (SELECT * FROM data_agg) TO '~/test_dummy_data/people_agg_tsfm.parquet' (FORMAT PARQUET)
+COPY (SELECT * FROM data_agg) TO '@dest_file' (FORMAT PARQUET)
 ;
 
 -- check parquet output
 SELECT *
-FROM read_parquet('~/test_dummy_data/people_agg_tsfm.parquet')
+FROM read_parquet('@dest_file')
 ;
