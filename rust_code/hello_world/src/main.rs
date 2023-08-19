7d27e4d719d903a4ace09f70b79826859bc4f64f
Matt <mattmartin14@gmail.com>
2023-08-19 15:33:53 -0400

    updated

--- rust_code/hello_world/src/main.rs
+++ rust_code/hello_world/src/main.rs
@@ -4,6 +4,8 @@
     cd into the project_folder_name > there should be a folder called "src" with a "main.rs" file
     -- open that file, edit, save
     -- then in terminal "cargo run"
+    -- to compile a binary, do "cargo build" ; this makes a binary in the target/debug folder
+    -- to make a more optimized release build, do "cargo build --release"; this makes a binary in target/release folder
 */
 
 fn main() {
---
+++ rust_code/hello_world/target/release/deps/hello_world-3763da5c27648a0e.d
@@ -0,0 +1,5 @@
+/Users/matthewmartin/dream_machine/rust_code/hello_world/target/release/deps/hello_world-3763da5c27648a0e: src/main.rs
+
+/Users/matthewmartin/dream_machine/rust_code/hello_world/target/release/deps/hello_world-3763da5c27648a0e.d: src/main.rs
+
+src/main.rs:
