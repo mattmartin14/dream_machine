55ea46db5b3241f719556b82685f7acaec7354f1
Matt <mattmartin14@gmail.com>
2023-08-19 15:35:45 -0400

    updated

--- rust_code/hello_world/src/main.rs
+++ rust_code/hello_world/src/main.rs
@@ -10,4 +10,9 @@
 
 fn main() {
     println!("Hello, world!");
+    test();
+}
+
+fn test() {
+    println!("test test")
 }
---
+++ rust_code/hello_world/target/debug/deps/hello_world-b3b0a2ce7a4fa34a.d
@@ -0,0 +1,5 @@
+/Users/matthewmartin/dream_machine/rust_code/hello_world/target/debug/deps/hello_world-b3b0a2ce7a4fa34a.rmeta: src/main.rs
+
+/Users/matthewmartin/dream_machine/rust_code/hello_world/target/debug/deps/hello_world-b3b0a2ce7a4fa34a.d: src/main.rs
+
+src/main.rs:
