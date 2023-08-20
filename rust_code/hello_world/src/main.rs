b1b3933cca7fbbf55c7ae0b275f9bc5a094eea60
Matt <mattmartin14@gmail.com>
2023-08-20 06:23:38 -0400

    updated

--- rust_code/hello_world/src/main.rs
+++ rust_code/hello_world/src/main.rs
@@ -8,6 +8,8 @@
     -- to make a more optimized release build, do "cargo build --release"; this makes a binary in target/release folder
 
     -- to link other files, the functions have to be marked "pub"
+    --  Then you add in the main.rs a section up top to import the module using "mod [name of other module without rs extension]"
+    -- functions in other files don't need to be uppercase to be recognized
 
 */
 mod test2;
