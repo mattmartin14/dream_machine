0a121489478848d182eb4cb3b6a5af066b3daf97
Matt <mattmartin14@gmail.com>
2023-09-02 06:44:29 -0400

    updated

--- benchmarks/write_speed/c_lang/c_writer_buffer_v2.c
+++ benchmarks/write_speed/c_lang/c_writer_buffer_v2.c
@@ -3,25 +3,32 @@
 #include <time.h>
 #include <string.h>
 
-#define TOT_ROWS 100000
-#define BUFFER_SIZE 100
+#define TOT_ROWS 10000000
 
 /*
-    When the buffer size is too small, it fails; need to figure out the proper way to track all this
-
-    Wonder if my define is throwing it off?
+    to do: 
+    got code working for single write;
+    got function working to build a buffer string
+    -- need to test using the buffer string
 
 */
 
 // to compile run in terminal clang -o c_writer c_writer.c
+
+int build_fmt_string(char *fmt_string, int num_elements) {
+    for (int i=1;i<=num_elements;i++) {
+        strcat(fmt_string, "%d\n");
+    }
+    return 0;
+}
+
 int main() {
-    
+
     clock_t start_ts, end_ts;
     double elapsed;
 
     start_ts = clock();
 
-    //int buffer_size = 8192*10;
 
     const char* home_dir = getenv("HOME");
     const char* file_sub_path = "/test_dummy_data/write_benchmark/c_generated.csv";
@@ -43,23 +50,44 @@ int main() {
 
     // takes 40 seconds with this buffered approach
     
-    char buffer[BUFFER_SIZE];
+    char buffer[4096*2*2*2];
     int buffer_index = 0;
 
-    for (int i = 0; i < TOT_ROWS; i++) {
-        int n = snprintf(buffer + buffer_index, BUFFER_SIZE - buffer_index, "%d\n", i);
+    char fmt_string_1_step[] = "%d\n";
+    
+    
+    char fmt_string_10_step[10*4];
 
-        if (n < 0 || n + buffer_index >= BUFFER_SIZE) {
+    build_fmt_string(fmt_string_10_step,10);
+
+
+    printf("fmt buffer of 10 looks like %s. \n",fmt_string_10_step);
+
+    int step_by = 1;
+
+    int max_buffer_size = sizeof(buffer);
+
+    int buffer_padding = 200;
+
+    int flush_cnt = 0;
+
+    for (int i = 0; i < TOT_ROWS; i+=step_by) {
+
+        int chars_written = snprintf(buffer + buffer_index, max_buffer_size - buffer_index, fmt_string_1_step, i+1);
+
+        if (chars_written < 0 || chars_written >= max_buffer_size - buffer_index) {
             printf("row was at %d\n",i);
             fprintf(stderr, "Error adding number to the buffer\n");
             return 1;
         }
 
-        buffer_index += n;
+        buffer_index += chars_written;
 
-        if (buffer_index >= BUFFER_SIZE - 1) {
+        //write out if we are close to the buffer padding so we dont hit overflow errors
+        if (buffer_index >= max_buffer_size - buffer_padding) {
             fputs(buffer, file);
             buffer_index = 0;
+            flush_cnt++;
         }
 
     }
@@ -67,6 +95,7 @@ int main() {
     if (buffer_index > 0) {
             fputs(buffer, file);
             buffer_index = 0;
+            flush_cnt++;
     }
 
     fclose(file);
@@ -75,5 +104,6 @@ int main() {
 
     elapsed = (double)(end_ts - start_ts)/ CLOCKS_PER_SEC;
 
-    printf("C Lang process complete. Total Run time to generate %d: %.2f seconds\n", TOT_ROWS, elapsed);
+    printf("C Lang process complete. Total buffer flushes: %d. Total Run time to generate %d: %.2f seconds\n"
+    , flush_cnt, TOT_ROWS, elapsed);
 }
