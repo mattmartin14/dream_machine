1030597d3b395adc0c0f4a9f95eddea91a957f53
Matt <mattmartin14@gmail.com>
2023-09-17 14:11:43 -0400

    updated

--- bash/http.sh
+++ bash/http.sh
@@ -7,15 +7,21 @@
     had to install wget and jq
 '
 
-file="test_res.json"
+
 url="https://api.chucknorris.io/jokes/random"
 
+#wget version
 #wget -q -O $file $url
 
-#curl version of command:
-curl -s $url -o $file
-
+#you can output the results to a file
 #get the joke (element "value" in the json response)
-joke=$(jq -r '.value' $file)
+#file="test_res.json"
+#curl -s $url -o $file
+#joke=$(jq -r '.value' $file)
+#echo $joke
 
+#this just outputs the one value we want to a variable all in one line
+#uses a combo of curl piping results to jq to parse
+joke=$(curl -s $url | jq -r '.value')
 echo $joke
+
