d441861d991c60420eaba39c42063f1ea1e5e2f2
Matt <mattmartin14@gmail.com>
2023-10-08 06:44:45 -0400

    updated

--- python/bq/gbq_streaming_insert.py
+++ python/bq/gbq_streaming_insert.py
@@ -7,18 +7,34 @@ def stream_insert():
     client = bigquery.Client(project=project_id)
 
     #config the payload here
-    payload = {"order_id":3, "order_date": "2023-06-05", "order_lines":
+    orders = []
+
+    order_1 = {"order_id":3, "order_date": "2023-06-05", "order_lines":
                 [
                     {"line_nbr":1, "item_id":833},
                     {"line_nbr":2,"item_id":921}
                 ]
             }
+    
+    orders.append(order_1)
+
+    order_2 = {"order_id":4, "order_date": "2022-01-12", "order_lines":
+                [
+                    {"line_nbr":1, "item_id":552},
+                    {"line_nbr":2,"item_id":389},
+                    {"line_nbr":3,"item_id":246}
+                ]
+            }
+    
+    orders.append(order_2)
 
     table_id = "{0}.ds1_test.orders_test".format(project_id)
 
-    errs = client.insert_rows_json(table_id, [payload])
+    errs = client.insert_rows_json(table_id, orders)
     if errs != []:
         print('errors while attempting insert: {0}'.format(errs))
+    else:
+        print('orders loaded to target')
 
 if __name__ == "__main__":
     stream_insert()
