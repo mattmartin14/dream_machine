762b5e5e9df8e79e350cf5b2ea3bd0831d0a24a4
Matt <mattmartin14@gmail.com>
2023-11-17 14:44:35 -0600

    updated

---
+++ go_code/excel/go.mod
@@ -0,0 +1,15 @@
+module excel
+
+go 1.21.1
+
+require (
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
+	github.com/xuri/efp v0.0.0-20230802181842-ad255f2331ca // indirect
+	github.com/xuri/excelize/v2 v2.8.0 // indirect
+	github.com/xuri/nfp v0.0.0-20230819163627-dc951e3ffe1a // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+)
---
+++ go_code/excel/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	// ran go get github.com/xuri/excelize/v2
+	"github.com/xuri/excelize/v2"
+)
+
+// to build the binary: go build -o excel main.go
+func main() {
+	// Create a new Excel file
+	file := excelize.NewFile()
+
+	// Create a new sheet in the Excel file
+	sheet := "Sheet1"
+	//index := file.NewSheet(sheet)
+
+	// Set some values in specific cells
+	file.SetCellValue(sheet, "A1", "Hello")
+	file.SetCellValue(sheet, "B1", "World!")
+	file.SetCellValue(sheet, "A2", 3.14)
+	file.SetCellValue(sheet, "B2", 42.0)
+	file.SetCellValue(sheet, "E1", "abcdefg")
+
+	// Save the Excel file
+	err := file.SaveAs("output.xlsx")
+	if err != nil {
+		fmt.Println("Error saving Excel file:", err)
+		return
+	}
+
+	fmt.Println("Excel file created successfully.")
+}
