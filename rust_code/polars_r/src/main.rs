708cd304c03ed08fb47f6787e35023cdf58c4411
Matt <mattmartin14@gmail.com>
2024-04-07 17:35:05 -0400

    updated

--- rust_code/polars_r/src/main.rs
+++ rust_code/polars_r/src/main.rs
@@ -1,27 +1,34 @@
 
-use polars::prelude::*;
+/*
+    Author: Matt Martin
+    Date: 4/7/24
+    Desc: Testing Polars on Rust
+*/
 
-fn main() {
+
+use polars::prelude::*;
+use std::error::Error;
+use std::env;
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    let home_dir = env::var("HOME")?;
+    let csv_f_path = format!("{}/test_dummy_data/fd/fin_data_1.csv", home_dir);
 
     //read the csv
-    let csv_f_path = "/Users/matthewmartin/test_dummy_data/fd/fin_data_1.csv";
-    let mut df = load_csv_to_df(csv_f_path)
-        .unwrap_or_else(|e| {
-            eprintln!("Error loading CSV: {}", e);
-            std::process::exit(1)
-        });
+    let df = load_csv_to_df(&csv_f_path)?;
     
     // view the schema
     //println!("{:?}", df.schema());
     
     /*
-        A few gotchas here:
+        A few gotchas here on how to use the transform:
             1) Polars Rust no longer supports just the direct dataframe grouping from the docs.
                 Instead you have to use the lazy evaluator
             2) to make the result come back as a DataFrame and not a LazyFrame, invoke the collect() function
             3) Collect will return a dataframe + polars error if applicable:
                 see https://docs.rs/polars/latest/polars/prelude/struct.LazyFrame.html#method.collect
-                to do this right, you have to add the expect() at the end
+                to do this right, you have to add the expect() or unwrap() at the end
 
     */
     //transform
@@ -32,26 +39,29 @@ fn main() {
             col("NetWorth").sum().alias("tot_net_worth")
         ])
         .collect()
-        .expect("Error getting dataframe created")
+        .unwrap()
+        //.expect("Error getting dataframe created")
     ;
 
-    // Display the result
-    println!("{:?}", df2);
+    //sample top 5 rows
+    println!("{}", df2.head(Some(5)));
 
 
     // export result to parquet
-    let par_f_path = "/Users/matthewmartin/test_dummy_data/fd/rust_par.parquet";
-    export_to_parquet(&mut df2, par_f_path);
-
+    let par_f_path = format!("{}/test_dummy_data/fd/rust_par.parquet", home_dir);
+    export_to_parquet(&mut df2, &par_f_path)?;
+    
+    Ok(())
 }
 
-//loads the CSV
-fn load_csv_to_df(csv_f_path: &str) -> PolarsResult<DataFrame> {
+// import the csv
+fn load_csv_to_df(csv_f_path: &str) -> Result<DataFrame, PolarsError> {
     CsvReader::from_path(csv_f_path)?.has_header(true).finish()
 }
 
-// pushes the dataframe to a parquet file
-fn export_to_parquet(df: &mut DataFrame, par_f_path: &str) {
-    let mut file = std::fs::File::create(par_f_path).unwrap();
-    ParquetWriter::new(&mut file).finish(df).unwrap();
+//exports the dataframe to parquet
+fn export_to_parquet(df: &mut DataFrame, par_f_path: &str) -> Result<(), PolarsError> {
+    let mut file = std::fs::File::create(par_f_path)?;
+    ParquetWriter::new(&mut file).finish(df)?;
+    Ok(())
 }
