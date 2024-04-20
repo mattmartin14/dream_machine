cedd5b5421838655e21c026dd8877c07d1a9e252
Matt <mattmartin14@gmail.com>
2024-04-20 13:49:29 -0400

    updated

---
+++ rust_code/rust_vs_c/c_code/looper.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+
+    int i = 0;
+    long long int z = 0;
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock(); // Record the start time
+
+    for (i = 1; i <= 1000000000; i++) {
+        z += i;
+    }
+
+    end = clock(); // Record the end time
+
+    cpu_time_used = ((double) (end - start)) * 1000.0 / CLOCKS_PER_SEC; // Calculate elapsed time in milliseconds
+
+    printf("accumulated value is %lld\n", z);
+    printf("Time to compute: %f milliseconds\n", cpu_time_used);
+
+    return 0;
+}
