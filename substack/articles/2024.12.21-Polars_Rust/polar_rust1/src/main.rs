1c57fb74476969188718f6f7935a3db411c83d2f
Matt <mattmartin14@gmail.com>
2024-12-22 19:22:36 -0500

    updated

--- substack/articles/2024.12.21-Polars_Rust/polar_rust1/src/main.rs
+++ substack/articles/2024.12.21-Polars_Rust/polar_rust1/src/main.rs
@@ -1,83 +1,54 @@
-
-/*
-    Author: Matt Martin
-    Date: 12/19/24
-    Desc: Polars/Rust/GCS Test
-*/
-
-
-use polars::prelude::*;
-use std::error::Error;
+use google_cloud_storage::client::Client;
+use google_cloud_storage::client::ClientConfig;
+use google_cloud_storage::http::Error;
+use google_cloud_storage::http::objects::upload::{Media, UploadObjectRequest, UploadType};
 use std::env;
+use std::fs::File;
+use std::io::Read;
 
-fn main() -> Result<(), Box<dyn Error>> {
+// need to add in polars to create the data frame
+// save it locally
+// then push to gcs
+// wonder if we can pull in the delta package and write to delta lake format here?
+// maybe also create a gcs bucket
 
-    // let home_dir = env::var("HOME")?;
-    // let csv_f_path = format!("{}/test_dummy_data/fd/fin_data_1.csv", home_dir);
+#[tokio::main]
+async fn main() -> Result<(), Error> {
 
-    //read raw data
-    //let df = load_csv_to_df(&csv_f_path)?;
-    
-    let lf = make_dummy_lf();
-   
-    //transform
-    let mut df = lf
-        .group_by([col("txn_dt")])
-        .agg([
-            col("last_nm").count().alias("lm_cnt"),
-            col("tot_amt").sum().alias("tot_net_worth")
-        ])
-        .collect()
-        .expect("Error getting dataframe created")
-    ;
+    let curr_dir = env::var("PWD").expect("Error getting current directory");
+    let par_f_path = format!("{}/tmp/data.parquet", curr_dir);
+    let gcs_key_path = "testing/abc/blah/data.parquet";
 
-    //sample top 5 rows
-    println!("{}", df.head(Some(5)));
+    upload_file_to_gcs(&par_f_path, &gcs_key_path).await?;
 
-
-    // export result to parquet
-    let bucket = env::var("GCS_BUCKET")?;
-    let gcs_dir = format!("gs://{}/polars_rust_warehouse/",bucket);
-    let par_f_path = format!("{}/rust_par.parquet", gcs_dir);
-    export_to_parquet(&mut df, &par_f_path)?;
-    
     Ok(())
 }
 
-// // import the csv
-// fn load_csv_to_df(csv_f_path: &str) -> Result<DataFrame, PolarsError> {
-//     CsvReader::from_path(csv_f_path)?.has_header(true).finish()
-// }
+async fn upload_file_to_gcs(local_f_path: &str, gcs_key_path: &str) -> Result<(), Error>{
 
-fn make_dummy_lf() -> LazyFrame {
-    let txn_dt = Series::new(
-        "txn_dt".into(),
-        [
-            "2024-12-10", "2024-12-11", "2024-12-12", "2024-12-13", 
-            "2024-12-14", "2024-12-15", "2024-12-16", "2024-12-17",
-            "2024-12-18", "2024-12-19",
-        ],
-    );
-    let tot_amt = Series::new(
-        "tot_amt".into(), 
-        [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
-    );
-    let last_nm = Series::new(
-        "last_nm".into(), 
-        [
-            "Smith", "Johnson", "Williams", "Brown", "Jones", 
-            "Garcia", "Miller", "Davis", "Rodriguez", "Martinez",
-        ],
-    );
+    // Create client.
+    let config = ClientConfig::default().with_auth().await.unwrap();
+    let client = Client::new(config);
 
-    let df = DataFrame::new(vec![txn_dt.into(), tot_amt.into(), last_nm.into()]).unwrap();
-    df.lazy()
-}
+    let bucket_nm = env::var("GCS_BUCKET").expect("Error retrieving bucket");
 
+    // read the local file into byte stream to upload
+    let mut f = File::open(local_f_path).expect("failed to open file");
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer).expect("error reading file");
 
-//exports the dataframe to parquet
-fn export_to_parquet(df: &mut DataFrame, par_f_path: &str) -> Result<(), PolarsError> {
-    let mut file = std::fs::File::create(par_f_path)?;
-    ParquetWriter::new(&mut file).finish(df)?;
+    //stupid rust borrowring, or a skill issue on my part :P
+    let owned_gcs_key_path = gcs_key_path.to_owned();
+
+    // Upload the file
+    let upload_type = UploadType::Simple(Media::new(owned_gcs_key_path));
+    let _uploaded = client.upload_object(&UploadObjectRequest {
+        bucket: bucket_nm,
+        ..Default::default()
+    }, buffer, &upload_type).await;
+    
+
     Ok(())
+
 }
+
