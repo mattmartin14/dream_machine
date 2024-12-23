a244ce755c2e891e9675f7219474f914f11b9b34
Matt <mattmartin14@gmail.com>
2024-12-22 20:45:33 -0500

    updated

--- substack/articles/2024.12.21-Polars_Rust/polar_rust1/src/main.rs
+++ substack/articles/2024.12.21-Polars_Rust/polar_rust1/src/main.rs
@@ -4,31 +4,24 @@ use google_cloud_storage::http::Error;
 use google_cloud_storage::http::objects::upload::{Media, UploadObjectRequest, UploadType};
 use std::env;
 use polars::prelude::*;
-//use std::fs::File;
-//use std::io::Read;
-//use std::fs::read;
-
-// need to add in polars to create the data frame
-// save it locally
-// then push to gcs
-// wonder if we can pull in the delta package and write to delta lake format here?
-// maybe also create a gcs bucket
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
 
-    let curr_dir = env::var("PWD").expect("Error getting current directory");
-    let par_f_path = format!("{}/tmp/data.parquet", curr_dir);
-    let gcs_key_path = "testing/abc/blah2/data.parquet";
+    // read raw csv data into dataframe
+    let home_dir = env::var("HOME").expect("Error retrieving home directory");
+    let csv_f_path = format!("{}/test_dummy_data/rust/data*.csv", home_dir);
+    let lf = read_csv(&csv_f_path);
 
-    let lf = make_dummy_lf();
-    
+    println!("Read csv raw data into dataframe");
+
     //transform
     let mut df = lf
-        .group_by([col("txn_dt")])
+        .group_by([col("ref_dt")])
         .agg([
-            col("last_nm").count().alias("lm_cnt"),
-            col("tot_amt").sum().alias("tot_net_worth")
+            col("first_name").count().alias("fm_cnt"),
+            col("last_name").count().alias("lm_cnt"),
+            col("net_worth").sum().alias("tot_net_worth")
         ])
         .collect()
         .expect("Error getting dataframe created")
@@ -36,11 +29,15 @@ async fn main() -> Result<(), Error> {
 
     //sample top 5 rows
     println!("{}", df.head(Some(5)));
+
+    let curr_dir = env::var("PWD").expect("Error getting current directory");
+    let par_f_path = format!("{}/tmp/data.parquet", curr_dir);
     
     export_to_parquet(&mut df, &par_f_path).expect("error exporting dataframe to parquet");
 
     let client = create_gcs_client().await;
 
+    let gcs_key_path = "testing/abc/blah2/data.parquet";
     upload_file_to_gcs(&client, &par_f_path, &gcs_key_path).await?;
 
     Ok(())
@@ -73,33 +70,16 @@ async fn upload_file_to_gcs(client: &Client, local_f_path: &str, gcs_key_path: &
 
 }
 
-fn make_dummy_lf() -> LazyFrame {
-    let txn_dt = Series::new(
-        "txn_dt".into(),
-        [
-            "2024-12-10", "2024-12-11", "2024-12-12", "2024-12-13",
-            "2024-12-14", "2024-12-15", "2024-12-16", "2024-12-17",
-            "2024-12-18", "2024-12-19",
-        ],
-    );
-    let tot_amt = Series::new(
-        "tot_amt".into(),
-        [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
-    );
-    let last_nm = Series::new(
-        "last_nm".into(),
-        [
-            "Smith", "Johnson", "Williams", "Brown", "Jones",
-            "Garcia", "Miller", "Davis", "Rodriguez", "Martinez",
-        ],
-    );
-
-    let df = DataFrame::new(vec![txn_dt.into(), tot_amt.into(), last_nm.into()]).unwrap();
-    df.lazy()
-}
 
 fn export_to_parquet(df: &mut DataFrame, par_f_path: &str) -> Result<(), PolarsError> {
     let mut file = std::fs::File::create(par_f_path)?;
     ParquetWriter::new(&mut file).finish(df)?;
     Ok(())
+}
+
+fn read_csv(file_path: &str) -> LazyFrame {
+    LazyCsvReader::new(file_path)
+        .with_has_header(true)
+        .finish()
+        .expect("Failed to read CSV file into LazyFrame")
 }
