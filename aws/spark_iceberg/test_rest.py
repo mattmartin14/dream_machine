020e57bc1d55df6202698bdb1d24d853d4e0e726
Matt <mattmartin14@gmail.com>
2025-09-27 06:30:04 -0400

    updated

--- aws/spark_iceberg/test_rest.py
+++ aws/spark_iceberg/test_rest.py
@@ -30,7 +30,7 @@ spark = (SparkSession.builder.appName('osspark')
     .getOrCreate()
 )
 
-s3_warehouse_path = 's3://matt-sbx-bucket-1-us-east-1/icehouse/'
+s3_warehouse_path = f's3://{os.getenv("aws_bucket")}/icehouse/'
 glue_db_name = 'icebox1'
 
 spark.sql(f"select * from {glue_db_name}.iceberg_test limit 5").show()
@@ -41,5 +41,74 @@ spark.sql(f"select * from {glue_db_name}.iceberg_test limit 5").show()
 #spark.sql(f"create table {catalog_name}.{glue_db_name}.iceberg_test2 (id bigint, data string) using iceberg location '{s3_warehouse_path}/iceberg_test2'")
 
 spark.sql(f"insert into {catalog_name}.{glue_db_name}.iceberg_test2 values(1, 'a'), (2, 'b')").show()
+
+spark.sql(f"update {catalog_name}.{glue_db_name}.iceberg_test2 set data = 'c' where id = 1")
+print("update worked")
+
+#test delete
+spark.sql(f"delete from {catalog_name}.{glue_db_name}.iceberg_test2 where id = 2")
+print("delete worked")
+
+#test ctas
+spark.sql(f"""create or replace table {catalog_name}.{glue_db_name}.iceberg_test3 
+          using iceberg
+          location '{s3_warehouse_path}/iceberg_test3'
+          as 
+          select * from {catalog_name}.{glue_db_name}.iceberg_test2
+""")
+
+print("ctas worked")
+
+spark.sql(f"drop table if exists {catalog_name}.{glue_db_name}.iceberg_test4")
+spark.sql(f"drop table if exists {catalog_name}.{glue_db_name}.iceberg_test5")
+
+spark.sql(f"""create table {catalog_name}.{glue_db_name}.iceberg_test4 
+          (id int, val string)
+          using iceberg
+          location '{s3_warehouse_path}/iceberg_test4'
+""")
+
+print('ctas 2 worked')
+
+spark.sql(f"insert into {catalog_name}.{glue_db_name}.iceberg_test4 values(1, 'x'), (2, 'y'), (3, 'z')")
+
+spark.sql(f"""create table {catalog_name}.{glue_db_name}.iceberg_test5
+          (id int, val string)
+          using iceberg
+          location '{s3_warehouse_path}/iceberg_test5'
+""")
+
+spark.sql(f"insert into {catalog_name}.{glue_db_name}.iceberg_test5 values(2, 'y2'), (3, 'z2'), (4, 'w')")
+
+
+#test merge
+merge_sql = f"""
+    MERGE INTO {catalog_name}.{glue_db_name}.iceberg_test4 as tgt
+        USING {catalog_name}.{glue_db_name}.iceberg_test5 as src
+            ON tgt.id = src.id
+    WHEN MATCHED THEN UPDATE SET tgt.val = src.val
+    WHEN NOT MATCHED THEN INSERT (id, val) VALUES (src.id, src.val)
+"""
+
+spark.sql(merge_sql)
+print("merge worked")
+
+spark.sql(f"select * from {catalog_name}.{glue_db_name}.iceberg_test2 limit 5").show()
+
+# this ctas doesnt work
+spark.sql(f"""create or replace table {catalog_name}.{glue_db_name}.iceberg_test6
+          using iceberg
+          location '{s3_warehouse_path}/iceberg_test6'
+          as
+          with vals as (
+             select 1 as id, 'a' as data
+             union all
+             select 2 as id, 'b' as data
+             union all
+             select 3 as id, 'c' as data
+          )
+          select * from vals
+
+""")
 
 spark.stop()
